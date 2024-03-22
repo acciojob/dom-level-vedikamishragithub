05f15cbd7103f8f2680a9391b32cc7fd35f41c82
Vedika Mishra <151787416+vedikamishragithub@users.noreply.github.com>
2024-03-22 19:57:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+let childElement = document.getElementById('level');
+let parentElement = childElement.parentNode;
+let i;
+for(i=1; parentElement != null; i++){
+    parentElement = parentElement.parentNode;
+}
+alert(`The level of the element is: ${i-1}`);
